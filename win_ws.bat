96b081bf54bfa94974e8ed868f298322c667fdd8
Joel Handwell <joelhandwell@gmail.com>
2015-07-08 17:57:43 -0400

    Organize Packages

--- win_ws.bat
+++ win_ws.bat
@@ -1,32 +1,33 @@
-choco install 7zip -y
-choco install adobereader -y
-choco install ccleaner -y
-choco install chefdk -y
-choco install chocolatey -y
-choco install ConEmu -y
-choco install eclipse -y
-choco install filezilla -y
-choco install gimp -y
-choco install git -y
-choco install GoogleChrome -y
-choco install hashcheck -y
+REM Windows 7
+choco install MicrosoftSecurityEssentials -y
 choco install ie11 -y
+
+REM Utility
+choco install GoogleChrome -y
+choco install adobereader -y
 choco install javaruntime -y
 choco install jdk8 -y
-choco install jre8 -y
-choco install kdiff3 -y
-choco install MicrosoftSecurityEssentials -y
-choco install packer -y
-choco install PowerShell -y
-choco install procexp -y
-choco install putty -y
-choco install skype -y
-choco install SublimeText3 -y
-choco install synergy -y
-choco install teamviewer -y
-choco install telegram -y
-choco install telegram.install -y
-choco install vagrant -y
-choco install vim -y
-choco install virtualbox -y
+choco install 7zip -y
 choco install Wget -y
+choco install ccleaner -y
+choco install procexp -y
+choco install hashcheck -y
+choco install teamviewer -y
+
+REM SysAdmin
+choco install PowerShell -y
+choco install ConEmu -y
+choco install putty -y
+choco install lili -y
+
+REM Coding
+choco install vim -y
+choco install SublimeText3 -y
+choco install kdiff3 -y
+choco install git -y
+
+REM DevOps
+choco install virtualbox -y
+choco install vagrant -y
+choco install packer -y
+choco install chefdk -y
