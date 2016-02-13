6b5a35c58efee5412432b8e4463ced60d21ac21b
Joel Handwell <joelhandwell@gmail.com>
2016-02-13 17:07:21 -0500

    Change order of environmental variable for choco

--- win_ws.bat
+++ win_ws.bat
@@ -1,3 +1,18 @@
+REM Performance settings. Execute after creating RAM drive D
+MKDIR D:\Windows\TEMP
+MKDIR D:\Users\%USERNAME\AppData\Local\Temp
+SETX TMP D:\Users\%USERNAME\AppData\Local\Temp
+SETX TEMP D:\Users\%USERNAME\AppData\Local\Temp
+
+MKDIR D:\Users\%USERNAME\AppData\Roaming
+SETX APPDATA D:\Users\%USERNAME\AppData\Roaming
+
+MKDIR D:\Users\%USERNAME\AppData\Local
+SETX LOCALAPPDATA D:\Users\%USERNAME\AppData\Local
+
+REM prepare chocolatey installation directory https://github.com/chocolatey/choco/wiki/Installation#before-you-install
+SETX ChocolateyInstall D:\ProgramData\Chocolatey
+
 REM Utility
 choco install google-chrome-x64 -y
 choco install adobereader -y
@@ -26,15 +41,3 @@ choco install terraform -y
 choco install chefdk -y
 
 SETX GEM_PATH C:/opscode/chefdk/embedded/lib/ruby/gems/2.1.0
-
-REM Performance settings. Execute after creating RAM drive D
-MKDIR D:\Windows\TEMP
-MKDIR D:\Users\%USERNAME\AppData\Local\Temp
-SETX TMP D:\Users\%USERNAME\AppData\Local\Temp
-SETX TEMP D:\Users\%USERNAME\AppData\Local\Temp
-
-MKDIR D:\Users\%USERNAME\AppData\Roaming
-SETX APPDATA D:\Users\%USERNAME\AppData\Roaming
-
-MKDIR D:\Users\%USERNAME\AppData\Local
-SETX LOCALAPPDATA D:\Users\%USERNAME\AppData\Local
