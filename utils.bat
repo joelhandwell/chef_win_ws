f53e49d3feb221348e089a0453e19dfe73798d80
Joel Handwell <joelhandwell@gmail.com>
2016-03-10 15:47:05 -0500

    create utils.bat

---
+++ utils.bat
@@ -0,0 +1,11 @@
+REM Heavy Guys
+choco install google-chrome-x64 -y
+choco install adobereader -y
+choco install teamviewer -y
+
+REM Utility
+choco install 7zip -y
+choco install Wget -y
+choco install hashcheck -y
+choco install linkshellextension -y
+choco install hosts.editor -y
