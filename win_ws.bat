3c833e9a3dea718ebf3d9a8ebaf1bd92061a0afc
Joel Handwell <joelhandwell@gmail.com>
2016-07-15 20:39:56 -0400

    Simplify main installation file

--- win_ws.bat
+++ win_ws.bat
@@ -1,45 +1,19 @@
 REM Run chocolatey.bat first if you do not installed chocolatey
 
-REM if you want to install them into D:, E: etc, replace C: to other drive letters.
-
 REM SysAdmin
-choco install ConEmu -y -ia 'APPLICATIONFOLDER=C:\Conemu'
+choco install ConEmu -y
 choco install putty -y
 choco install rsync -y
 choco install clink -y
 choco install vim -y
 
-REM Sublime (Custom Location)
-REM choco install sublimetext3 -y -ia '/DIR=C:\Sublime'
-
-REM Sublime (Default Location)
+REM DevOps tools for Chef
 choco install sublimetext3 -y
-
-REM Git (Custom Location)
-REM MKDIR C:\Git
-REM choco install git.install -y -ia '/DIR=C:\Git' -params '/GitAndUnixToolsOnPath'
-
-REM Git (Default Location)
 choco install git.install -y -params '/GitAndUnixToolsOnPath'
 choco install kdiff3 -y
-
-REM VirtualBox
-MKDIR C:\Oracle\VirtualBox
-MKDIR C:\Users\%USERNAME%\VirtualBoxVMs
-SET ProgramFiles(x86)=C:/ && choco install virtualbox -y -ia 'INSTALLDIR=C:\Oracle\VirtualBox'
-SETX /M PATH "%PATH%;C:\Oracle\VirtualBox"
-VBoxManage setproperty machinefolder C:\Users\%USERNAME%\VirtualBoxVMs
-
-REM Vagrant
-MKDIR C:\HashiCorp\Vagrant
-MKDIR C:\Users\%USERNAME%\.vagrant.d
-SETX VAGRANT_HOME C:\Users\%USERNAME%\.vagrant.d
-choco install vagrant -y -ia 'VAGRANTAPPDIR=C:\HashiCorp\Vagrant'
-
-REM ChefDK
-MKDIR C:\opscode
-choco install chefdk -y -ia 'INSTALLLOCATION=C:\opscode'
-SETX GEM_PATH C:\opscode\chefdk\embedded\lib\ruby\gems\2.1.0
+choco install virtualbox -y
+choco install vagrant -y
+choco install chefdk -y
 
 REM Other Devops tools
 choco install packer -y
