acbdbac659faed7d30db7154422eece9a5411317
Joel Handwell <joelhandwell@gmail.com>
2017-05-16 08:37:43 -0700

    Organize something I use and do not use

--- utils.bat
+++ utils.bat
@@ -1,15 +1,18 @@
 REM Heavy Guys
 choco install google-chrome-x64 -y
 choco install adobereader -y
-choco install teamviewer -y
-choco install ngrok.portable -y
+choco install libreoffice -y
+choco install gimp -y
+choco install inkscape -y
 
 REM Utility
+choco install ccleaner -y
 choco install 7zip -y
 choco install Wget -y
 choco install hashcheck -y
 choco install linkshellextension -y
 choco install hosts.editor -y
+choco install greenshot -y
 
 REM AWS related
 choco install python -y
