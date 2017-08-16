360f9ad178089366cccf3fceb6d4e042a691754d
Joel Handwell <joelhandwell@gmail.com>
2017-08-15 21:29:02 -0700

    Create init.bat

---
+++ init.bat
@@ -0,0 +1,3 @@
+REM Init
+choco install ccleaner 7zip hashcheck hosts.editor greenshot ConEmu Wget putty rsync clink vim sublimetext3 kdiff3 -y
+choco install git.install -y -params '/GitAndUnixToolsOnPath'
