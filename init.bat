8eaaf7130288b309279a373392ff2543567bf1f1
Joel Handwell <joelhandwell@gmail.com>
2017-08-15 22:09:01 -0700

    Fix bug and add windirstat

--- init.bat
+++ init.bat
@@ -1,3 +1,3 @@
 REM Init
-choco install ccleaner 7zip fastcopychoco install hashcheck hosts.editor greenshot ConEmu Wget putty rsync clink vim sublimetext3 kdiff3 -y
+choco install ccleaner 7zip windirstat hashcheck hosts.editor greenshot ConEmu Wget putty rsync clink vim sublimetext3 kdiff3 -y
 choco install git.install -y -params '/GitAndUnixToolsOnPath'
