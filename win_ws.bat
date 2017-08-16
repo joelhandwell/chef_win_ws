caeff7d202b0da88c11186ef963d9ede11fe3ac0
Joel Handwell <joelhandwell@gmail.com>
2017-08-15 21:28:19 -0700

    Separate light ones which is installed initially

--- win_ws.bat
+++ win_ws.bat
@@ -1,15 +1,6 @@
 REM Run chocolatey.bat first if you do not installed chocolatey
 
-REM SysAdmin
-choco install ConEmu -y
-choco install putty -y
-choco install rsync -y
-choco install clink -y
-choco install vim -y
-
 REM DevOps tools for Chef
-choco install sublimetext3 -y
-choco install git.install -y -params '/GitAndUnixToolsOnPath'
 choco install kdiff3 -y
 choco install virtualbox -y
 choco install vagrant -y
